da2ca96bf5308f78053eb57a2a73248a23f648e8
haohaolee <hayatelee@gmail.com>
2014-07-01 23:44:32 +0800

    update for surrounded regions

--- surrounded-regions.cpp
+++ surrounded-regions.cpp
@@ -2,41 +2,54 @@
 #include <queue>
 #include <vector>
 #include <utility>
+#include <iostream>
 
 using namespace std;
 
 class Solution {
     set<pair<size_t, size_t> > points_set;
-    size_t rows;
-    size_t columns;
 
 public:
     void solve(vector<vector<char>> &board) {
-        rows = board.size();
-        columns = board[0].size();
+        size_t rows = board.size();
+        if (rows <= 2)
+            return;
+
+        size_t columns = board[0].size();
+
+        if (columns <= 2)
+            return;
+
         for(size_t i = 0; i < columns; ++i)
         {
             process(board, 0, i);
             process(board, rows-1, i);
         }
-        if (rows > 2)
+
+        for(size_t j = 1; j < rows - 1; ++j)
         {
-            for(size_t j = 1; j < rows - 1; ++j)
-            {
-                process(board, j, 0);
-                process(board, j, columns - 1);
-            }
+            process(board, j, 0);
+            process(board, j, columns - 1);
         }
+
+        for(size_t i = 1; i < rows - 1; ++i)
+            for(size_t j = 1; j < columns - 1; ++j)
+            {
+                if(board[i][j] == 'O'
+                   && points_set.find(make_pair(i, j)) ==  points_set.end())
+                    board[i][j] = 'X';
+            }
+
     }
 
 private:
-    void process(vector<vector<char> > &board, size_t i, size_t j)
+    void process(vector<vector<char> > &board, size_t row, size_t column)
     {
-        char c =  board[i][j];
+        char c =  board[row][column];
         if (c == 'O'
-            && points_set.find(make_pair(i , j) == points_set.end()))
+            && points_set.find(make_pair(row , column)) == points_set.end())
         {
-           BFS(board, i, j);
+           BFS(board, row, column);
         }
     }
 
@@ -68,8 +81,37 @@ private:
 
     bool isValid(vector<vector<char> > &board, int x, int y)
     {
-
+        size_t rows = board.size();
+        size_t columns = board[0].size();
+        if (x < 0 || y < 0 || x >= rows || y >= columns)
+            return false;
+        if (board[x][y] == 'O' &&
+            points_set.find(make_pair(x, y)) == points_set.end())
+            return true;
+        else
+            return false;
     }
 };
 
+int main()
+{
+    vector<vector<char> > v = {
+        { 'X', 'O', 'X', 'O', 'X', 'O' },
+        { 'O', 'X', 'O', 'X', 'O', 'X' },
+        { 'X', 'O', 'X', 'O', 'X', 'O' },
+        { 'O', 'X', 'O', 'X', 'O', 'X' }
+    };
 
+    Solution s;
+    s.solve(v);
+
+    for(auto &row : v)
+    {
+        for(auto &i : row)
+        {
+            cout << i << ' ';
+        }
+        cout << '\n';
+    }
+
+}
